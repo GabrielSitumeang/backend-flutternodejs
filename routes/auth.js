0572566c061517f7496e75b317ace983604d72f1
Sadrakh Situmeang <sadrakhsitumeang2004@gmail.com>
2024-09-18 15:44:00 +0700

    create signin api endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 authRouter.post('/api/signup',async(req, res)=>{
     try{
@@ -24,4 +25,31 @@ authRouter.post('/api/signup',async(req, res)=>{
     }
 });
 
+//signin api endpoint
+authRouter.post('/api/signin', async(req, res)=>{
+    try{
+        const {email, password} = req.body;
+        const findUser = await User.findOne({email});
+        if(!findUser){
+            return res.status(400).json({msg: "User not found with this email"});
+        }else {
+            const isMatch = await bcrypt.compare(password, findUser.password);
+            if(!isMatch){
+                return res.status(400).json({msg:'Incorrect Password'});
+            } else {
+                const token = jwt.sign({id:findUser._id}, "passwordKey");
+                
+                //remove sensitive information 
+                const {password, ...userWithoutPassword } = findUser._doc;
+
+                //send the response
+                res.json({token, ...userWithoutPassword });
+            }
+        }
+    } catch(e){
+        res.status(500).json({error:e.message});
+    }
+})
+
 module.exports = authRouter;
+
